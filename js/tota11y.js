e4bc8f734fbd6d640975ef55b456207080ab7a61
BRBoer <braxtonrboer@gmail.com>
2016-07-29 11:24:41 -0700

    Revert "change variables"
    This reverts commit a1ba79bd4ace148de02ec7d7b8e66dcf937e2420.

--- js/tota11y.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-
-            var tota11y = document.createElement('SCRIPT');
-            tota11y.type = 'text/javascript';
-            tota11y.src = '//khan.github.io/tota11y/tota11y/build/tota11y.min.js';
-            document.getElementsByTagName('head')[0].appendChild(tota11y);
-
-        }
